80ec2741cf6153d560e8f86c9146a81f44db6e76
Hiromichi Cho <hiromichi.cho@hiromichic-wsm.internal.salesforce.com>
2017-07-13 16:33:31 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std:
+
+int main() {
+
+    static_assert("C++17");
+    cout << "Hello, world" << endl;
+    int a[3] = { 0, 1, 2 };
+    a[3] = 4;
+
+    return 0;
+}
